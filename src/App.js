2822e7bf5a2173f4ca3808ed2072b18bafae5af2
Yifei Wang <hello.yifei@icloud.com>
2017-05-28 16:59:44 +0200

    added input test before implementation

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+// import Button from './Button.js';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      namevalue: '',
-      emailvalue:''
-    }
-    this.handleInput = this.handleInput.bind(this);
-  }
-  handleInputname(event){
-    this.setState({
-      namevalue: event.target.value
-    })
-  }
-  handleInputemail(event){
-    this.setState({
-      emailvalue: event.target.value
-    })
-  }
-  render() {
-    return (
-      <div className="App">
-       <input placeholder='name' value={this.state.namevalue} onChange={this.handleInput}/>
-       <input placeholder='email' value={this.state.emailvalue} onChange={this.handleInputemail}/>
-      </div>
-    );
-  }
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       namevalue: '',
+//       emailvalue:''
+//     }
+//     this.handleInput = this.handleInput.bind(this);
+//   }
+//   handleInputname(event){
+//     this.setState({
+//       namevalue: event.target.value
+//     })
+//   }
+//   handleInputemail(event){
+//     this.setState({
+//       emailvalue: event.target.value
+//     })
+//   }
+render() {
+  return (
+    <div className="App">
+    
+    </div>
+  )
+}
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
 import App from './App';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders input', () => {
+  let wrapper = shallow(<App />);
+  const input = <input />;
+  expect(wrapper.contains(input)).toBe(true);
+
 });

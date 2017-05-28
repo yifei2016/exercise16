c0cd9515d59b08aeb8f01bb97b365a556d6f2fc4
Yifei Wang <hello.yifei@icloud.com>
2017-05-28 17:57:51 +0200

    implemented state value of empty string

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      namevalue: '',
+      emailvalue:''
+    }
 
+  }
 render() {
   return (
     <div className="App">
-    <input placeholder='name'   />
-    <input placeholder='email'  />
+    <input placeholder='name'  value={this.state.namevalue} />
+    <input placeholder='email'  value={this.state.emailvalue} />
     </div>
   )
 }
